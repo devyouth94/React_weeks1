ebf53ab386886d03a133e89e891141df51cad229
devyouth94 <devyouth94@gmail.com>
2022-07-22 21:27:49 +0900

    reset css적용, 컴포넌트 폴더 구조 생성,  헤더 기본 골격 생성

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-wrap">
+        <h1>My Todo List</h1>
+        <p>React</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import Header from "../header/header";
+
+const Layout = () => {
+  return (
+    <Header/>
+  )
+}
+
+export default Layout;
